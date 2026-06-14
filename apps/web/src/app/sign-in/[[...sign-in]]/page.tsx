dec924e0f2e338913d1749b1dd0dda8c1e6185a4
RJX001 <rajanpatel2001@hotmail.com>
2026-06-14 19:00:51 +0100

    ss

--- apps/web/src/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <main className="flex min-h-[70vh] items-center justify-center px-4 py-10">
-      <SignIn />
+      <SignIn path="/sign-in" routing="path" />
     </main>
   );
 }
